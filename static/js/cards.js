c186f6c2e91440b9289d28e99f5b3da5f221e7db
Zackggiitt <161786105+Zackggiitt@users.noreply.github.com>
2024-05-17 11:44:09 +0800

    Delete static directory

--- static/js/cards.js
+++
@@ -1,9 +0,0 @@
-//Changes the colour of each request status based on the text in the .status class field.
-const request_status = document.querySelectorAll('.status'); 
-request_status.forEach(request_status => {
-  if (request_status.textContent.trim() === "Complete") {
-    request_status.classList.add('green');
-  } else if (request_status.textContent.trim() === "Incomplete") {
-    request_status.classList.add('blue');
-  }
-});
--- static/js/popupForm.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('openFormButton').addEventListener('click', function() {
-    document.getElementById('popupForm').style.display = 'block';
-});
-
-document.getElementById('closeFormButton').addEventListener('click', function() {
-    document.getElementById('popupForm').style.display = 'none';
-});
